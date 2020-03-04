802742697d6d1894d6b0fe533004f3e1ab7e004e
erezrokah <erezrokah@users.noreply.github.com>
2020-03-04 16:18:41 +0100

    chore: prepare repo

--- seed.js
+++ seed.js
@@ -6,14 +6,15 @@ title: VALUE
 ---
 `;
 
-const count = process.env.SEED_FILE_COUNT || 100;
+const count = process.env.SEED_FILE_COUNT || 3000;
 
 const start = () => {
   const dir = path.join(__dirname, 'content', 'posts');
   fs.mkdirSync(dir, { recursive: true });
   for (let i = 0; i < count; i++) {
+    const id = `${i}`.padStart(`${count}`.length, '0');
     fs.writeFileSync(
-      path.join(dir, `post-${i}.md`),
+      path.join(dir, `post-${id}.md`),
       template.replace('VALUE', `Title ${i}`),
     );
   }
